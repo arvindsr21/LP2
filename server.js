b69de7fee6a030271a0394ba46b7a99e3e3e145a
arvindsr21 <desaimrahul@gmail.com>
2024-05-10 13:21:38 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import {app} from './app.js';
+
+// const DB = 'mongodb://localhost:27017/wadl'
+// mongoose
+//     .connect(DB)
+//     .then(() => console.log('Database connected successfully'))
+//     .catch(err => console.log('Error connecting Database',err));
+
+// const publicDirectoryPath = path.join(__dirname, '');
+
+const PORT = 5500
+const server = app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
+})
